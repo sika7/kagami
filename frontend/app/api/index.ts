64263b359a5662018e51d975c5521a15d190c024
sika7 <91676209+sika7@users.noreply.github.com>
2025-05-03 21:23:52 +0900

    フロントエンドディレクトリを削除

--- frontend/app/api/index.ts
+++
@@ -1,9 +0,0 @@
-// API クライアント
-export { default as apiClient } from './client';
-export { queryClient } from './query-client';
-
-// 認証関連
-export * from './auth';
-
-// ユーティリティ
-export * from './utils';
